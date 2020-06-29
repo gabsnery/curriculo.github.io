5424bbc986e663c6a426909609c36b8d83f67ee2
Gabriela Nery <gneri94@gmail.com>
2020-06-28 23:14:54 -0300

    Updates

---
+++ precache-manifest.d94f652c4a640f0a0f47a0297677ccd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97bb9bf02ea2140e81aac3c3c42e05a",
+    "url": "/curriculo.github.io/index.html"
+  },
+  {
+    "revision": "fe82cd47a12edcff672b",
+    "url": "/curriculo.github.io/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "6738d981a4858e585dc2",
+    "url": "/curriculo.github.io/static/js/2.a6d8fd33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/curriculo.github.io/static/js/2.a6d8fd33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe82cd47a12edcff672b",
+    "url": "/curriculo.github.io/static/js/main.e2d9778b.chunk.js"
+  },
+  {
+    "revision": "87238d2d04ea8d2c2d5b",
+    "url": "/curriculo.github.io/static/js/runtime-main.3db10c35.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/curriculo.github.io/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea339b3d00049ab49ea71e7ecb0e7ce8.js"
+  "/curriculo.github.io/precache-manifest.d94f652c4a640f0a0f47a0297677ccd9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/curriculo.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
